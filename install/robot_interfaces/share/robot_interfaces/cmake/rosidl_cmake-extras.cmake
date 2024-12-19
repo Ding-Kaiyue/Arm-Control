52985329d04a60214aa313d56e2523bc360ed572
Ding Kaiyue <57055551+Ding-Kaiyue@users.noreply.github.com>
2024-12-19 10:40:14 +0800

    Delete install directory

--- install/robot_interfaces/share/robot_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(robot_interfaces_IDL_FILES "msg/QtRecv.idl;msg/LineMsg.idl;msg/ArmState.idl")
-set(robot_interfaces_INTERFACE_FILES "msg/QtRecv.msg;msg/LineMsg.msg;msg/ArmState.msg")
